3cd51375dd715d712401e08da8dec59f66a23812
Sushma Rao <raosushma14@gmail.com>
2019-09-21 15:51:23 -0400

    basic skeleton is ready .

--- TABot/Bots/EchoBot.cs
+++ TABot/Bots/EchoBot.cs
@@ -3,19 +3,87 @@
 //
 // Generated with Bot Builder V4 SDK Template for Visual Studio EchoBot v4.5.0
 
+using System;
 using System.Collections.Generic;
 using System.Threading;
 using System.Threading.Tasks;
+using Microsoft.Azure.CognitiveServices.Language.LUIS.Runtime.Models;
 using Microsoft.Bot.Builder;
 using Microsoft.Bot.Schema;
+using Microsoft.Extensions.Logging;
+using TABot.Services.BotServices;
+using System.Linq;
+using TABot.Helpers;
 
 namespace TABot.Bots
 {
     public class EchoBot : ActivityHandler
     {
+        private IBotServices _botServices;
+        private ILogger<EchoBot> _logger;
+
+        public EchoBot(IBotServices botServices, ILogger<EchoBot> logger)
+        {
+            _botServices = botServices ?? throw new ArgumentNullException(nameof(botServices));
+            _logger = logger ?? throw new ArgumentNullException(nameof(logger));
+        }
+
         protected override async Task OnMessageActivityAsync(ITurnContext<IMessageActivity> turnContext, CancellationToken cancellationToken)
         {
-            await turnContext.SendActivityAsync(MessageFactory.Text($"Echo: {turnContext.Activity.Text}"), cancellationToken);
+            var dispatchResult = await _botServices.Dispatch.RecognizeAsync(turnContext, cancellationToken);
+            var topIntent = dispatchResult.GetTopScoringIntent();
+
+            await DispatchToTopIntentAsync(turnContext, topIntent.intent, dispatchResult, cancellationToken);
+        }
+
+        private async Task DispatchToTopIntentAsync(ITurnContext<IMessageActivity> turnContext, string intent, RecognizerResult dispatchResult, CancellationToken cancellationToken)
+        {
+            switch (intent)
+            {
+                case "l_TALuis":
+                    await ProcessLuisAsync(turnContext, dispatchResult.Properties["luisResult"] as LuisResult, cancellationToken);
+                    break;
+                case "q_TAKB":
+                    await ProcessQnAAsync(turnContext, cancellationToken);
+                    break;
+                default:
+                    await turnContext.ReplyTextAsync("Sorry! I am unable to help with that at the moment.");
+                    break;
+            }
+        }
+
+        private async Task ProcessQnAAsync(ITurnContext<IMessageActivity> turnContext, CancellationToken cancellationToken)
+        {
+            var answers = await _botServices.QnA.GetAnswersAsync(turnContext);
+
+            if (answers.Any())
+            {
+                await turnContext.SendActivityAsync(MessageFactory.Text(answers.First().Answer),cancellationToken);
+            }
+            else
+            {
+                await turnContext.ReplyTextAsync("Sorry! I am unable to answer that at the moment.");
+            }
+       
+        }
+
+        private async Task ProcessLuisAsync(ITurnContext<IMessageActivity> turnContext, LuisResult luisResult, CancellationToken cancellationToken)
+        {
+            var result = luisResult.ConnectedServiceResult;
+            var topIntent = result.TopScoringIntent.Intent;
+
+            switch (topIntent)
+            {
+                case "ErrorQuestions":
+                    await turnContext.ReplyTextAsync("I can help with error");
+                    break;
+                case "AssignmentQuestions":
+                    await turnContext.ReplyTextAsync("i can help with your assignment");
+                    break;
+                default:
+                    await turnContext.ReplyTextAsync("Sorry! I am unable to understand that");
+                    break;
+            }
         }
 
         protected override async Task OnMembersAddedAsync(IList<ChannelAccount> membersAdded, ITurnContext<IConversationUpdateActivity> turnContext, CancellationToken cancellationToken)
---
+++ TABot/Services/BotServices/BotServices.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.Bot.Builder.AI.Luis;
+using Microsoft.Bot.Builder.AI.QnA;
+using Microsoft.Extensions.Configuration;
+
+namespace TABot.Services.BotServices
+{
+    public class BotServices : IBotServices
+    {
+        public BotServices(IConfiguration configuration)
+        {
+            Dispatch = new LuisRecognizer(
+                new LuisApplication(
+                    configuration["DispatchLuisAppId"],
+                    configuration["DispatchLuisAPIKey"],
+                    configuration["DispatchLuisAPIHostName"]), 
+                new LuisPredictionOptions {
+                    IncludeAllIntents = true,
+                    IncludeInstanceData = true
+                },
+                true);
+
+            ErrorLuis = new LuisRecognizer(
+                new LuisApplication(
+                    configuration["ErrorLuisAppId"],
+                    configuration["ErrorLuisAPIKey"],
+                    configuration["ErrorLuisAPIHostName"]),
+                new LuisPredictionOptions
+                {
+                    IncludeAllIntents = true,
+                    IncludeInstanceData = true
+                },
+                true);
+
+            QnA = new QnAMaker(new QnAMakerEndpoint
+            {
+                KnowledgeBaseId = configuration["QnAKnowledgebaseId"],
+                EndpointKey = configuration["QnAEndpointKey"],
+                Host = configuration["QnAEndpointHostName"]
+            });
+        }
+        public LuisRecognizer Dispatch { get; private set; }
+
+        public QnAMaker QnA { get; private set; }
+
+        public LuisRecognizer ErrorLuis { get; private set; }
+    }
+}
